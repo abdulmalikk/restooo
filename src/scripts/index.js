fbe5cb1d30f0b8dbda8c30a6a70342d69fab94fa
abdul <malik301002@gmail.com>
2020-10-23 17:26:36 +0700

    membuat drawer ketika di mobile beserta fungsinya

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,18 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import '../styles/responsive.css';
 
-console.log('Hello Coders! :)');
+const drawerToggler = document.querySelector('#drawertoggler');
+const drawerOverlay = document.querySelector('.drawer-overlay');
+const drawer = document.querySelector('.drawer');
+
+drawerToggler.addEventListener('click', function (event) {
+  drawer.classList.toggle('open');
+  drawerOverlay.style.display = 'block';
+  event.stopPropagation();
+});
+
+drawerOverlay.addEventListener('click', function () {
+  drawer.classList.remove('open');
+  this.style.display = 'none';
+});
