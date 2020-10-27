74584ee967e0a8134b02c81f55064f5ab2d5729a
abdul <malik301002@gmail.com>
2020-10-27 09:54:36 +0700

    memperbaiki responsive dan aksesibilitas website

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,5 @@
 import 'regenerator-runtime'; /* for async await transpile */
+import data from '../DATA.json';
 import '../styles/main.css';
 import '../styles/responsive.css';
 
@@ -15,4 +16,29 @@ drawerToggler.addEventListener('click', function (event) {
 drawerOverlay.addEventListener('click', function () {
   drawer.classList.remove('open');
   this.classList.remove('open');
+});
+
+document.addEventListener('keydown', ({ key }) => {
+  if (key === 'Escape') {
+    drawer.classList.remove('open');
+    drawerOverlay.classList.remove('open');
+  };
+});
+
+const listRestoElm = document.querySelector('.list-resto-items');
+const listResto = data.restaurants;
+listResto.forEach((restaurant) => {
+  const truncatedDesc = restaurant.description.substring(125, length);
+  listRestoElm.innerHTML += `
+    <div class="card">
+      <div class="card-rating">★ ${restaurant.rating}</div>
+      <img src="${restaurant.pictureId}" alt="${restaurant.name}" width="100%">
+      <div class="card-body">
+        <h1 class="card-location">Lokasi : ${restaurant.city}</h1>
+        <h1 class="card-title">${restaurant.name}</h1>
+        <p class="card-desc">${truncatedDesc} . . .</p>
+      </div>
+      <a href="#" class="btn-card-detail">Lihat detail</a>
+    </div>
+  `;
 });
